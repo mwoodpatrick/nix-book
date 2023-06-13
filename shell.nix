3c1cf0e7289798dfb1085ca029bb47cdf9fa0ef0
Amy de Buitléir <amy@nualeargais.ie>
2023-06-13 21:14:26 +0100

    restructure repo

--- shell.nix
+++ shell.nix
@@ -2,6 +2,8 @@ with (import <nixpkgs> {});
 mkShell {
   buildInputs = [
     asciidoctor
-    python311Packages.pygments
+    # python311Packages.pygments don't need to install, asciidoctor includes it
+    # rubyPackages.coderay
+    # rubyPackages.rouge
   ];
 }
