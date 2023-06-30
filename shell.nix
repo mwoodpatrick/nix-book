cb11a9a64a55bcdad4e92b63b30fdf88780fbab2
Amy de Buitléir <amy@nualeargais.ie>
2023-06-30 19:05:01 +0100

    migrated run-code-inline

--- shell.nix
+++ shell.nix
@@ -1,6 +1,10 @@
 with (import <nixpkgs> {});
+let
+   run-code-inline = (builtins.getFlake git+https://codeberg.org/mhwombat/run-code-inline).packages.${builtins.currentSystem}.default;
+in
 mkShell {
   buildInputs = [
+    run-code-inline
     asciidoctor
     # python311Packages.pygments don't need to install, asciidoctor includes it
     # rubyPackages.coderay
