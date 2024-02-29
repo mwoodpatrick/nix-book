06a0958a07eac7d8a688ac051165028346c77836
Amy de Buitléir <amy@nualeargais.ie>
2024-02-29 19:26:37 +0000

    epub experiment

--- shell.nix
+++ shell.nix
@@ -5,7 +5,8 @@ in
 mkShell {
   buildInputs = [
     run-code-inline
-    asciidoctor
+    # asciidoctor
+    asciidoctor-with-extensions
     # python311Packages.pygments don't need to install, asciidoctor includes it
     # rubyPackages.coderay
     # rubyPackages.rouge
